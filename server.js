f6633f913c8484d4d27f5d75f55d5e43172118db
derekjpeters <derek.j.peters@gmail.com>
2024-12-10 17:55:37 -0500

    updated with CRUD features

--- server.js
+++ server.js
@@ -1,8 +1,70 @@
 console.log("Node.js is running");
 //Import the Express Library
 const express = require("express");
+//import body parser to process the request through express
+const bodyParser = require('body-parser');
 //Create an instance of the Expess Application
 const app = express();
+const PORT = 3000;
+
+//Parsing for Middleware
+app.use(bodyParser.json());
+
+//Sample Data
+let hobbits = [
+	{id: 1, name: "Frodo", age: 30},
+	{id: 2, name: "Samwise", age: 27}
+];
+
+app.get('/hobbits', (req,res) => {
+	res.json(hobbits);
+})
+
+app.post('/hobbits', (req, res) => {
+	let newHobbit = {
+		id: hobbits.length + 1,
+		name: req.body.name,
+		age: req.body.age,
+	};
+	hobbits.push(newHobbit);
+	res.status(201).json(newHobbit);
+})
+
+app.put('/hobbits/:id', (req, res) => {
+	//Extract the id parameter from the request url - convert to an integer
+	const id = parseInt(req.params.id);
+
+	//find the hobbit with the matching id
+	const hobbit = hobbits.find(h => h.id === id);
+
+	//something if no id is found, we will want to send a 404 request with an error message
+	if (!hobbit) {
+		return res.status(404).json({error: "Hobbit not found"});
+	}
+
+	//Update the name
+	//If no new provided, keep the existing name
+	hobbit.name = req.body.name || hobbit.name;
+
+	//Update the age
+	//If no new age is provided, keep the existing age
+	hobbit.age = req.body.age || hobbit.age;
+
+	//third thing to keep in mind is you have to send the updated hobbit to the server as a JSON response
+	res.json(hobbit);
+});
+
+app.delete('/hobbits/:id', (req, res) => {
+	const id = parseInt(req.params.id);
+	const hobbitIndex = hobbits.findIndex(h => h.id === id);
+
+	if(hobbitIndex === -1) {
+		return res.status(404).json({error: "Hobbit not found"});
+	}
+	//Logic to delet hobbit record from server
+	const deletedHobbit = hobbits.splice(hobbitIndex, 1);
+	res.json(deletedHobbit);
+})
 
 //Define a route for the root URL
 //When a get request is made to the root URL, the callback function is executed
@@ -11,6 +73,8 @@ const app = express();
 app.get("/", (req, res) => {
 	res.send("Hello, Express!!");
 });
+
+
 
 app.get("/api/students", (req, res) => {
 	const students = [
@@ -44,13 +108,13 @@ app.get("/api/greet/:name", (req, res) => {
 	}
 });
 
-app.use((req, res) => {
-	res.status(404).send("Route not Found");
-});
+// app.use((req, res) => {
+// 	res.status(404).send("Route not Found");
+// });
 
 //Start the server and make it Listen for incoming request on port 3000
 //Callback function runs once the serer starts
-app.listen(3000, () => {
+app.listen(PORT, () => {
 	//Log a message to the console to indicate that the server is running
-	console.log("Server is running on http://localhost:3000");
+	console.log(`Server is running on http://localhost:${PORT}`);
 });
